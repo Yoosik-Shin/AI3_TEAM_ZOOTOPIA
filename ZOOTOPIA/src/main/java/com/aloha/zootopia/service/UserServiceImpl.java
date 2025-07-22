6888835de538d71f5d8905c62d4badfa5ca5636f
jjown55 <bomeonda99@gmail.com>
2025-07-22 17:34:07 +0900

    1234

--- ZOOTOPIA/src/main/java/com/aloha/zootopia/service/UserServiceImpl.java
+++ ZOOTOPIA/src/main/java/com/aloha/zootopia/service/UserServiceImpl.java
@@ -142,11 +142,20 @@ public class UserServiceImpl implements UserService {
     public int updateUser(Users user) throws Exception {
         return userMapper.updateUser(user);
     }
+    
 
     @Override
     public int deleteUserAuth(String email) throws Exception {
+
         return userMapper.deleteUserAuth(email);
     }
+
+    @Override
+    public void deleteById(Long userId) {
+        userMapper.deleteById(userId); // 실제 삭제
+        // 또는 user.setEnabled(0); 저장으로 soft-delete도 가능
+    }
+
 
 
     // 소셜 로그인 사용자 조회 및 생성
