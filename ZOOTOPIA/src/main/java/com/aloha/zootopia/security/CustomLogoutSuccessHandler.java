d8716e40f97585cfac34077e489491023ccacd0e
Yoosik-Shin <snorlax234678@gmail.com>
2025-07-22 15:35:53 +0900

    끝?

--- ZOOTOPIA/src/main/java/com/aloha/zootopia/security/CustomLogoutSuccessHandler.java
+++ ZOOTOPIA/src/main/java/com/aloha/zootopia/security/CustomLogoutSuccessHandler.java
@@ -2,7 +2,6 @@ package com.aloha.zootopia.security;
 
 import java.io.IOException;
 
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.web.authentication.logout.LogoutSuccessHandler;
 import org.springframework.stereotype.Component;
@@ -16,18 +15,11 @@ import lombok.extern.slf4j.Slf4j;
 @Component
 public class CustomLogoutSuccessHandler implements LogoutSuccessHandler {
 
-    @Value("${spring.security.oauth2.client.registration.naver.client-id}")
-    private String naverClientId;
-
     @Override
     public void onLogoutSuccess(HttpServletRequest request, HttpServletResponse response, Authentication authentication)
             throws IOException, ServletException {
         
-        log.info("소셜 로그아웃을 진행합니다...");
-
-        String naverLogoutUrl = "https://nid.naver.com/nidlogin.logout.html?client_id=" + naverClientId + "&service_url=http://localhost:8080/";
-
-        // 네이버 로그아웃 URL로 리디렉션
-        response.sendRedirect(naverLogoutUrl);
+        log.info("사용자 로그아웃 처리. 로그인 페이지로 리디렉션합니다.");
+        response.sendRedirect("/login?logout=true");
     }
 }
